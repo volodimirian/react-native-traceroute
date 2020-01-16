840b8cd7ec780ebcea68392f9443b6dd729908d9
Vladimir Smirnov <vlad@rightbalance.io>
2020-01-16 13:08:28 +0300

    created initialized plugin stuff.

---
+++ index.js
@@ -0,0 +1,36 @@
+import { NativeModules } from 'react-native';
+
+const { RNReactNativePing } = NativeModules;
+class Traceroute {
+  // /**
+  //  *
+  //  * Get RTT (Round-trip delay time)
+  //  *
+  //  * @static
+  //  * @param {string} ipAddress - For example : 8.8.8.8
+  //  * @param {Object} option - Some optional operations
+  //  * @param {number} option.timeout - timeout
+  //  * @returns
+  //  * @memberof Ping
+  //  */
+  // static async start(ipAddress, option) {
+  //   const result = await RNReactNativePing.start(ipAddress, option);
+  //   return result;
+  // }
+  // static async getTrafficStats() {
+  //   const result = await RNReactNativePing.getTrafficStats();
+  //   return result;
+  // }
+
+  /**
+   * @static
+   * @param {string} hostName host name that need to analyse tracerouting to
+   * @returns
+   * @memberof Traceroute
+   */
+  static async start(hostName) {
+    return 'some result';
+  }
+}
+
+export default Ping;
